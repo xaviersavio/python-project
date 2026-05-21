89609b811568b3bc711578b6c3051ad3601ff411
Xavier Fernandes <xfdes28@hotmail.com>
2026-05-22 01:32:23 +1200

    test

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/api/v1/details')
 def details():
     return jsonify({
-        "message": "Hello World ,this is serioud coding v2 ! dd  ! n",
+        "message": "Hello World ,this is serioud coding v2 ! dd gg  ! n",
         "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "hostname": socket.gethostname()
     })
